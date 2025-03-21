443ab4e8b7dee79e0c430db8e368c00c07a2fe69
Mert Kaan İrtegün <irtegun16@itu.edu.tr>
2025-03-21 18:53:37 +0300

    refactor: create validation helper

--- src/constants/enums.js
+++ src/constants/enums.js
@@ -1,7 +1,3 @@
-/**
- * Application-wide enums
- */
-
 export const Language = Object.freeze({
   EN: 'en',
   TR: 'tr'
@@ -10,4 +6,26 @@ export const Language = Object.freeze({
 export const Events = Object.freeze({
   LANGUAGE_CHANGED: 'language-changed',
   LANG_CHANGED: 'lang-changed'
+});
+
+export const EmployeeFields = Object.freeze({
+  FIRST_NAME: 'firstName',
+  LAST_NAME: 'lastName',
+  DATE_OF_EMPLOYMENT: 'dateOfEmployment',
+  DATE_OF_BIRTH: 'dateOfBirth',
+  PHONE_NUMBER: 'phoneNumber',
+  EMAIL: 'email',
+  DEPARTMENT: 'department',
+  POSITION: 'position'
+});
+
+export const Department = Object.freeze({
+  ANALYTICS: 'analytics',
+  TECH: 'tech'
+});
+
+export const Position = Object.freeze({
+  JUNIOR: 'junior',
+  MEDIOR: 'medior',
+  SENIOR: 'senior'
 }); 
